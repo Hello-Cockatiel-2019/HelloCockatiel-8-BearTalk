15a2584f2f014a517bf1314840a09aa25e97503b
wiree <wirawat09@gmail.com>
2019-11-06 17:17:47 +0700

    real

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Landing from './Landing';
+import Logo from './Logo';
+import Content5 from './Content5';
+import './App.css'
+
+import { Button } from 'react-bootstrap';
+
+import Carousel from 'react-bootstrap/Carousel'
+export default class App extends React.Component {
+  render() {
+    const resizeMode =  'center';
+    const text = 'This is some text inlaid in an <Image />';
+
+    return (
+      <div className="main" style={{backgroundColor:"#E5E5E5"}}>
+        <div class="container-fluid">
+          <Landing />
+          <Logo />
+          <Content5 />
+          <br />
+          <br/>
+          <br/>
+          <div  class="row" style={{backgroundImage:'URL(/images/BG3.png)', backgroundRepeat: "no-repeat", backgroundSize: "100%"}}>
+            <div   class="col-2 col-md-2 col-sm-2"><img src="/images/Logopng.png" width="100%" style={{marginLeft:"10%",marginTop:"10%"}} />
+            </div>
+            <div id="six1" class="row" style={{ width: "100%"}}>
+              <div class="col-12 col-md-12 col-sm-12"><img src="/images/All3.png" width="80%" style={{marginLeft:"10%",marginTop:"-10%"}} /></div>
+            </div>
+
+            <Logo/>
+
+          <div class="row" id="bottom" style={{ width: "100%" }}>
+            <div class="col-2 col-md-2 col-sm-2" ></div>
+            <div class="col-2 col-md-2 col-sm-2" ></div>
+            <div class="col-2 col-md-2 col-sm-2" style={{ marginLeft: "3%" }}>
+              <a href="https://www.facebook.com/SITHelloWorld/"><img src="/images/fb.png" alt="logo 512" width="200%" style={{ marginBottom: "-140%" }} /></a>
+            </div>
+          </div>
+         <img src="/images/Vector.png" alt="logo 512" width="100%"  />
+          </div>
+        </div>
+      </div >
+    )
+
+  }
+
+
+}
---
+++ src/Content5.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
+import Logo from './Logo';
+class Content5 extends React.Component {
+	render() {
+		return (
+			<div id="content5" style={{ backgroundImage: 'URL(/images/BG2.5.png)', backgroundRepeat: "no-repeat", backgroundSize: "100%" }} >
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col-2 col-md-2 col-sm-2"><AnchorLink href="#news"><img src="/images/Logopng.png" width="100%" style={{ marginLeft: "10%", marginTop: "10%" }} /></AnchorLink></div>
+					</div>
+					<br />
+					<br />
+					<br />
+					<br />
+					<br />
+					<br />
+					<br />
+					<br />
+					<br />
+					<br />
+					<br />
+					<br />
+					<br />
+					<br />
+					<br />
+					<br />
+					<br />
+					<br />
+					<br />
+					<br />
+					
+					<div class="row">
+
+						<div class="col-4 col-md-6 col-sm-4"><img src="/images/text.png" width="100%" style={{ marginLeft: "8%", marginTop: "-14%" }} /></div>
+						<div class="col-6 col-md-6 col-sm-4"><img src="/images/text2.png" width="80%" style={{ marginLeft: "16%", marginTop: "1%" }} /></div>
+
+					</div>
+
+					<br />
+
+					<Logo/>
+					<div id="srry">
+					<div class="col-12 col-md-12 col-sm-12"><img src="/images/srry.png" alt="logo 512" width="100%" /></div>
+					<Logo/>
+
+					</div>
+
+				</div>
+
+			</div>
+		);
+	}
+}
+
+export default Content5;
+
